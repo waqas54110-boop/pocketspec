ce13e9e08586fece8d4a093e509d5cc188a09fb9
waqas54110-boop <waqas54110@gmail.com>
2026-07-18 15:53:46 +0500

    create-safepay-session.js

--- api/create-safepay-session.js
+++ api/create-safepay-session.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
       mode: 'payment',
       currency: 'PKR',
       amount: Math.round(amount * 100),
-      metadata: { order_id: orderId, customer: customerName || '' }
+     metadata: { order_id: orderId }
     });
 
     res.status(200).json({ token: response.tracker.token, orderId });
