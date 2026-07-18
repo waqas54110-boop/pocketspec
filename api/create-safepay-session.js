f5ce9d1c62033875d7749759f3ce45c61bede31f
waqas54110-boop <waqas54110@gmail.com>
2026-07-18 17:51:35 +0500

    create-safepay-session.js

--- api/create-safepay-session.js
+++ api/create-safepay-session.js
@@ -6,6 +6,14 @@ const safepay = Safepay(process.env.SAFEPAY_SECRET_KEY, {
 });
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -29,7 +37,6 @@ module.exports = async (req, res) => {
     });
 
     const token = response.data.tracker.token;
-
     res.status(200).json({ token, orderId });
 
   } catch (err) {
