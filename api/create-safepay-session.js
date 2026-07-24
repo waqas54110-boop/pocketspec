7eb57a0f4c360e5673c97d10fe8f912ec2689c8e
waqas54110-boop <waqas54110@gmail.com>
2026-07-24 12:26:11 +0500

    create-safepay-session.js

--- api/create-safepay-session.js
+++ api/create-safepay-session.js
@@ -7,23 +7,10 @@ const safepay = Safepay(process.env.SAFEPAY_SECRET_KEY, {
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  // Debug: inspect available methods
-  console.log('safepay keys:', Object.keys(safepay));
-  console.log('safepay.auth:', safepay.auth);
-  console.log('safepay.checkouts:', safepay.checkouts);
 
   return res.status(200).json({
-    keys: Object.keys(safepay),
-    hasAuth: !!safepay.auth,
-    hasCheckouts: !!safepay.checkouts,
-    authKeys: safepay.auth ? Object.keys(safepay.auth) : null,
-    checkoutsKeys: safepay.checkouts ? Object.keys(safepay.checkouts) : null
+    checkoutKeys: safepay.checkout ? Object.keys(safepay.checkout) : null,
+    clientKeys: safepay.client ? Object.keys(safepay.client) : null,
+    paymentsKeys: safepay.payments ? Object.keys(safepay.payments) : null
   });
 };
